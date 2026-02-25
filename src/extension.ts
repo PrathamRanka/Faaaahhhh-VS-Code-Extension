59ca96d645a97dae45ff95f911672d4067345a98
Pratham <pranka0789@gmail.com>
2026-02-25 12:30:06 +0530

    working extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,43 +1,47 @@
 import * as vscode from "vscode";
 import * as path from "path";
-import * as fs from "fs";
+import * as os from "os";
 import { exec } from "child_process";
 
 let lastPlayed = 0;
 
 export function activate(context: vscode.ExtensionContext) {
+    function playSound() {
 
-    console.log("EXTENSION ACTIVATE CALLED");
+        const now = Date.now();
+        if (now - lastPlayed < 2500) return; 
+        lastPlayed = now;
 
-    vscode.window.showInformationMessage("FAAAAH EXTENSION ACTIVE");
-
-function playSound() {
-
-    console.log("playSound() CALLED");
-
-    const now = Date.now();
-    if (now - lastPlayed < 2500) return;
-    lastPlayed = now;
-
-    const audioPath = path.join(
-        context.extensionPath,
-        "media",
-        "fahhhhh.wav"
+        const audioPath = path.join(
+            context.extensionPath,
+            "media",
+            "fahhhhh.wav" 
+        );
+        if (os.platform() === "win32") {
+    exec(`powershell -c (New-Object Media.SoundPlayer "${audioPath}").PlaySync();`);
+        }
+        else if (os.platform() === "darwin") {
+            exec(`afplay "${audioPath}"`);
+        } 
+        else {
+            exec(`aplay "${audioPath}"`);
+        }
+    }
+    const testCommand = vscode.commands.registerCommand(
+        "faaaah.testSound",
+        () => {
+            playSound();
+        }
     );
 
-    console.log("Playing audio silently:", audioPath);
+    context.subscriptions.push(testCommand);
+    vscode.window.onDidEndTerminalShellExecution((e: any) => {
 
-    // Windows silent audio playback using PowerShell
-    exec(`powershell -c (New-Object Media.SoundPlayer "${audioPath}").PlaySync();`);
+        // exitCode !== 0 means failure
+        if (typeof e.exitCode === "number" && e.exitCode !== 0) {
+            playSound();
+        }
+    });
 }
 
-    //  TEST TRIGGER — force sound after 3 seconds
-    setTimeout(() => {
-        console.log("⏱ Timeout trigger firing");
-        playSound();
-    }, 3000);
-}
-
-export function deactivate() {
-    console.log("EXTENSION DEACTIVATED");
-}
+export function deactivate() {}
